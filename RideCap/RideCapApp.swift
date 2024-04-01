e6ae1c43063663da44557d6b2fdbd5da7e28c45b
Cameron Kania <camkania@gmail.com>
2024-04-01 16:48:51 -0400

    Initial Commit

---
+++ RideCap/RideCapApp.swift
@@ -0,0 +1,17 @@
+//
+//  RideCapApp.swift
+//  RideCap
+//
+//  Created by Cameron Kania on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RideCapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
