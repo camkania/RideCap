765f28bbec5a64060ba2a8040ff73a6fad332ff8
Cameron Kania <camkania@gmail.com>
2024-04-01 20:39:58 -0400

    Math has been implemented.
    Pretty major overhaul.
    
    Decided to do it based on dispatch interval instead of cycle time. (a bit complicated for this exercise ).
    
    The math works and the app is functional.

--- RideCap/ContentView.swift
+++ RideCap/ContentView.swift
@@ -9,42 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     // initalize variables
-    @State private var numberOfVehicles = 0
-    @State private var vehcicleCapacity = 0
+    let secondsPerHour = 3600.0
+    @State private var vehcicleCapacity = 12
+    @State private var dispatchInterval = 20.0
     
-    @State private var showTime = 0.0
-    @State private var loadTime = 0.0
-    @State private var unloadTime = 0.0
+    @FocusState private var dispatchIsFocused: Bool
     
-
-    
-    @FocusState private var showTimeIsFocused: Bool
-    @FocusState private var loadTimeIsFocused: Bool
-    @FocusState private var unloadTimeIsFocused: Bool
-    
-    var cycleTime: Double {
-       var totalTime = loadTime + showTime + unloadTime
-        return totalTime
-    }
     var thrc: Double {
         //Calcuate the thrc here
-        //let numOfRVs = numberOfVehicles
-        //let grandTotal = total
-        //let amountPerPerson = grandTotal / peopleCount
-        var capacity = 0.0
+        
+        //3600 seconds per hour / 20 second dispatch * 12 guests or 2,160 guest-per-hour
+        let capacity = (secondsPerHour / dispatchInterval) * Double(vehcicleCapacity)
+        
         return capacity
     }
-   
+    
     var body: some View {
         NavigationStack {
             Form {
                 Section("Vehicle Inputs"){
-                    Picker("Number of Vehicles", selection: $numberOfVehicles){
-                        ForEach(0..<101) {
-                            Text("\($0) RVs")
-                        }
-                        .pickerStyle(.navigationLink)
-                    }
                     Picker("Vehicle Capacity", selection: $vehcicleCapacity){
                         ForEach(0..<101) {
                             Text("\($0) riders")
@@ -54,73 +37,36 @@ struct ContentView: View {
                 }
                 
                 Section("Show Timing Inputs (in seconds)"){
+                    //Load Time input
                     HStack{
-                        Text("Load Time:")
-                        TextField("Enter amount", value: $loadTime, format: .number)
+                        Text("Dispatch Interval")
+                        TextField("Enter amount", value: $dispatchInterval, format: .number)
                             .textFieldStyle(RoundedBorderTextFieldStyle()) // To make TextField look like a picker
                             .multilineTextAlignment(.trailing)
-                           .padding(.horizontal) // Add some padding for better spacing
+                            .padding(.horizontal) // Add some padding for better spacing
                             .keyboardType(.decimalPad)
-                            .focused($loadTimeIsFocused)
-                    
-                    }
-                    .frame(height: 1) // Adjust height as needed
-                    
-                    HStack{
-                        Text("Show Time:")
-                        TextField("Enter amount", value: $showTime, format: .number)
-                            .textFieldStyle(RoundedBorderTextFieldStyle()) // To make TextField look like a picker
-                            .multilineTextAlignment(.trailing)
-                           .padding(.horizontal) // Add some padding for better spacing
-                            .keyboardType(.decimalPad)
-                            .focused($showTimeIsFocused)
-                    
-                    }
-                    .frame(height: 1) // Adjust height as needed
-                    
-                    HStack{
-                        Text("Unload Time:")
-                        TextField("Enter amount", value: $unloadTime, format: .number)
-                            .textFieldStyle(RoundedBorderTextFieldStyle()) // To make TextField look like a picker
-                            .multilineTextAlignment(.trailing)
-                           .padding(.horizontal) // Add some padding for better spacing
-                            .keyboardType(.decimalPad)
-                            .focused($unloadTimeIsFocused)
+                            .focused($dispatchIsFocused)
                     }
                     .frame(height: 1) // Adjust height as needed
                 }
-                Section("Cycle Time"){
-                    Text(thrc, format: .number)
-                    
-                }
+                
                 Section("Theoretical Hourly Ride Capacity (THRC)"){
-                    Text(thrc, format: .number)
-                    
-                }
-            }
-           
-            
-            .navigationTitle("Ride Capacity")
-            .toolbar {
-                if showTimeIsFocused{
-                    Button("Done") {
-                        showTimeIsFocused = false
+                        Text(thrc, format: .number)
                     }
                 }
-                if loadTimeIsFocused{
-                    Button("Done") {
-                        loadTimeIsFocused = false
+                
+                .navigationTitle("Ride Capacity")
+                .toolbar {
+                    if dispatchIsFocused{
+                        Button("Done") {
+                            dispatchIsFocused = false
+                        }
                     }
-                }
-                if unloadTimeIsFocused{
-                    Button("Done") {
-                        unloadTimeIsFocused = false
-                    }
+
                 }
             }
         }
     }
-}
 
 #Preview {
     ContentView()
